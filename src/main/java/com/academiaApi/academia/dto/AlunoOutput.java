2145414836392614ca7bdac4b1ee07db234979fc
Playerleleo <leonardogomesdossantos123@gmail.com>
2023-08-07 20:47:53 -0300

    getter and setter resolve

--- src/main/java/com/academiaApi/academia/dto/AlunoOutput.java
+++ src/main/java/com/academiaApi/academia/dto/AlunoOutput.java
@@ -1,12 +1,20 @@
 package com.academiaApi.academia.dto;
-
 import com.academiaApi.academia.model.Plano;
 import com.academiaApi.academia.model.Treino;
 import lombok.Data;
 
 import java.time.LocalTime;
 import java.util.UUID;
+
+
 @Data
-public record AlunoOutput(UUID id,String nome,Plano plano,Treino treino,LocalTime horario,
-                    Boolean ativo,String telefone,String email) {
+public class AlunoOutput {
+    UUID id;
+    String nome;
+    Plano plano;
+    Treino treino;
+    LocalTime horario;
+    Boolean ativo;
+    String telefone;
+    String email;
 }
--- src/main/java/com/academiaApi/academia/service/impl/AlunoImpl.java
+++ src/main/java/com/academiaApi/academia/service/impl/AlunoImpl.java
@@ -2,14 +2,43 @@ package com.academiaApi.academia.service.impl;
 
 import com.academiaApi.academia.dto.AlunoInput;
 import com.academiaApi.academia.dto.AlunoOutput;
+import com.academiaApi.academia.model.Aluno;
+import com.academiaApi.academia.repository.AlunoRepository;
 import com.academiaApi.academia.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class AlunoImpl implements AlunoService {
+    @Autowired
+    private AlunoRepository alunoRepository;
     @Override
     public AlunoOutput cadastrandoAluno(AlunoInput alunoInput) {
+        pegaInputAluno(alunoInput);
+
+
         return null;
     }
-    AlunoInput PegaInputAluno(AlunoInput alunoInput){
+    AlunoInput pegaInputAluno(AlunoInput alunoInput){
+        alunoInput.setNome(alunoInput.getNome());
+        alunoInput.setPlano(alunoInput.getPlano());
+        alunoInput.setTreino(alunoInput.getTreino());
+        alunoInput.setHorario(alunoInput.getHorario());
+        alunoInput.setAtivo(alunoInput.getAtivo());
+        alunoInput.setTelefone(alunoInput.getTelefone());
+        alunoInput.setEmail(alunoInput.getEmail());
         return alunoInput;
+    }
+
+    AlunoOutput respostaAluno(Aluno aluno){
+        var resposta = new AlunoOutput();
+        resposta.setId(aluno.getId());
+        resposta.setNome(aluno.getNome());
+        resposta.setPlano(aluno.getPlano());
+        resposta.setTreino(aluno.getTreino());
+        resposta.setHorario(aluno.getHorario());
+        resposta.setAtivo(aluno.getAtivo());
+        resposta.setTelefone(aluno.getTelefone());
+        resposta.setEmail(aluno.getEmail());
+
+        return resposta;
     }
 }
